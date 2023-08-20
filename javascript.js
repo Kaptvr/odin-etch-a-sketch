8619f5ce6cd7165a0c122c91720b52e4150d366e
Franek Sornat <62433571+Kaptvr@users.noreply.github.com>
2023-08-20 08:54:59 +0200

    Add 16x16 grid creator in js and css style

---
+++ javascript.js
@@ -0,0 +1,58 @@
+function main () {
+
+    createRows();
+    fillRows();
+}
+
+function createRows() {
+    for (let i = 0; i < 16; i++) {
+        const divWhole = document.querySelector('.divWhole'); //select hard-coded div
+        const divCreate = document.createElement('div');
+        divCreate.classList.add('row', `row${i}`);
+        divWhole.appendChild(divCreate);
+    }
+}
+
+function fillRows() {
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const divRow = document.querySelector(`.row${i}`);
+            const divCreate = document.createElement('div');
+            divCreate.classList.add('single');
+            divRow.appendChild(divCreate);
+        }
+    }
+}
+
+main();
+
+
+//Also works//
+
+// function main () {
+
+//     createRows();
+
+//     const divRow = document.querySelectorAll('.row');
+//     divRow.forEach( row => fillRows(row.classList[1])) //classList[1] == `row${i}` from createRows()
+// }
+
+// function createRows() {
+//     for (let i = 0; i < 16; i++) {
+//         const divWhole = document.querySelector('.divWhole');
+//         const divCreate = document.createElement('div');
+//         divCreate.classList.add('row', `row${i}`);
+//         divWhole.appendChild(divCreate);
+//     }
+// }
+
+// function fillRows(rowNum) {
+//     for (let i = 0; i < 16; i++) {
+//         const divRow = document.querySelector(`.${rowNum}`);
+//         const divCreate = document.createElement('div');
+//         divCreate.classList.add('single');
+//         divRow.appendChild(divCreate);
+//     }
+// }
+
+// main();
